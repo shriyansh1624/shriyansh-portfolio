07822da59963792202d4cd3a599b06eb6834da06
Shriyansh Sharma <en23cs301996@medicaps.ac.in>
2026-07-14 13:40:33 +0530

    upadted css code

--- routes/ai.js
+++ routes/ai.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 const { chatWithAI } = require("../controllers/aiController");
 
+router.use((req, res, next) => {
+    console.log("AI Route Hit:", req.method, req.originalUrl);
+    next();
+});
+
 router.post("/chat", chatWithAI);
 
 module.exports = router;
